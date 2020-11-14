331ff2daa0419d3150d0a00c8647b49afeb67140
apocalyvec <s-vector.lee@hotmail.com>
2020-11-14 17:36:29 -0500

    implement biLSTM with attention

--- test_biRNN_attention.py
+++ test_biRNN_attention.py
@@ -101,11 +101,11 @@ for scn, xy in scenario_data.items():
     y = encoder.transform(xy['y'].reshape(-1, 1)).toarray()
     x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.20, random_state=3, shuffle=True)
 
-    # test the ANN
-    history = build_train_ann(x_train, x_test, y_train, y_test)
-    plot_train_history(history, note=str(scn) + ' ANN')
+    # test the BIRNN_attention
+    history = build_train_birnn_with_attention(x_train, x_test, y_train, y_test)
+    plot_train_history(history, note=str(scn) + ' BIRNN_attention')
     eval = history.model.evaluate(x=x_test, y=y_test)
-    scenario_train_histories[('ANN', scn)] = [history, eval]
+    scenario_train_histories[('BIRNN_attention', scn)] = [history, eval]
 
     # test the RNN
     history = build_train_rnn(x_train, x_test, y_train, y_test)
@@ -113,95 +113,9 @@ for scn, xy in scenario_data.items():
     eval = history.model.evaluate(x=x_test, y=y_test)
     scenario_train_histories[('RNN', scn)] = [history, eval]
 
-    # test the CNN
-    history = build_train_cnn(x_train, x_test, y_train, y_test)
-    plot_train_history(history, note=str(scn) + ' CNN')
-    eval = history.model.evaluate(x=x_test, y=y_test)
-    scenario_train_histories[('CNN', scn)] = [history, eval]
-
-    # test the BIRNN_attention
-    history = build_train_birnn_with_attention(x_train, x_test, y_train, y_test)
-    plot_train_history(history, note=str(scn) + ' BIRNN_attention')
-    eval = history.model.evaluate(x=x_test, y=y_test)
-    scenario_train_histories[('BIRNN_attention', scn)] = [history, eval]
-
-
-strcts = {'ANN': build_train_ann, 'RNN': build_train_rnn, 'CNN': build_train_cnn, 'BIRNN_attention': build_train_birnn_with_attention}
 freqs = ['04g', '24g', '5g']
 locs = ['foot', 'hand', 'head']
-# horizontal axis are the frequencies, vertical are the locations
-for mdl in strcts.keys():
-    plot_cm_results(freqs, locs, [(key, value) for key, value in scenario_train_histories.items() if mdl in key], note=mdl)
-
-scenario_train_histories_without_model = ([mdl_scn, his_eval[0].history] for mdl_scn, his_eval in scenario_train_histories.items())
-# pickle.dump(scenario_train_histories, open('scenario_train_histories_10012020.p', 'wb'))
-
-
-
-
-
-
-
-
-
-
-# rename_map = {'foo': 'foot', 'han': 'hand', 'hea': 'head'}
-# scenario_train_histories_rename = dict()
-# for key, value in scenario_train_histories.items():
-#     key_rename = (key[0], (rename_map[key[1][0]], key[1][1]))
-#     scenario_train_histories_rename[key_rename] = value
-#
-# # plot scenario-based performance
-# width = 0.175
-# plt.rcParams["figure.figsize"] = (8, 4)
-# SMALL_SIZE = 12
-# MEDIUM_SIZE = 14
-# BIGGER_SIZE = 16
-#
-# plt.rc('font', size=SMALL_SIZE)  # controls default text sizes
-# plt.rc('axes', titlesize=SMALL_SIZE)  # fontsize of the axes title
-# plt.rc('axes', labelsize=MEDIUM_SIZE)  # fontsize of the x and y labels
-# plt.rc('xtick', labelsize=SMALL_SIZE)  # fontsize of the tick labels
-# plt.rc('ytick', labelsize=SMALL_SIZE)  # fontsize of the tick labels
-# plt.rc('legend', fontsize=SMALL_SIZE)  # legend fontsize
-# plt.rc('figure', titlesize=BIGGER_SIZE)
-# for scns in [locs, freqs]:
-#     strct_sns_acc_dict = dict([(model_strct, []) for model_strct in
-#                                strcts.keys()])  # i.e., ('ANN', [0.9, 0.8, 0.3]) in the list are the average accuracy for this sencario, sencario could be 04g
-#     for sn in scns:  # i.e., sn = 04g, scns = '04g', '24g', '5g', find marginal accuracy for all 04g
-#         for model_strct in strcts.keys():  # iterate through model structures: ANN, RNN, CNN
-#             sn_strct_accuracies = [hist_eval[1][1] for strct_scn, hist_eval in scenario_train_histories_rename.items()
-#                                    if strct_scn[0] == model_strct and sn in strct_scn[1]]
-#             sn_strct_acc = np.mean(sn_strct_accuracies)
-#             strct_sns_acc_dict[model_strct].append(sn_strct_acc)
-#     ind = np.arange(len(scns))
-#     for i, model_strct_scn_accs in enumerate(strct_sns_acc_dict.items()):
-#         model_strct, scn_accs = model_strct_scn_accs
-#         plt.bar(ind + i * width, scn_accs, width, label=model_strct)
-#         for j in range(len(scn_accs)):
-#             plt.text(ind[j] + i * width, scn_accs[j] + 0.05, str(round(scn_accs[j], 3)), horizontalalignment='center',
-#                      verticalalignment='center')
-#     plt.ylim(0.0, 1.1)
-#     plt.ylabel('Average Accuracy')
-#     plt.title('Accuracy by model structure and scenario')
-#     plt.xticks(ind + width / 2, scns)
-#     plt.legend(loc=4)
-#     plt.show()
-#
-# # plot scenarios-based training history
-# plt.rcParams["figure.figsize"] = (10, 10)
-#
-# fig, ax = plt.subplots(nrows=len(locs), ncols=len(freqs))
-# for i, lc in enumerate(locs):
-#     for j, fq in enumerate(freqs):
-#         strct_sn_hists = [(strct_scn[0], hist_eval[0].history['val_loss']) for strct_scn, hist_eval in
-#                           scenario_train_histories_rename.items() if (lc, fq) == strct_scn[1]]
-#         strct_sn_hists.reverse()
-#         for strct, sn_hist in strct_sn_hists:
-#             ax[i][j].plot(sn_hist, label=strct)
-#         ax[i][j].set_xlabel('Epoch')
-#         ax[i][j].set_ylabel('Validation loss')
-#         ax[i][j].set_ylim(-0.1, 3.0)
-#         ax[i][j].set_title(str(lc) + ', ' + str(fq))
-#         ax[i][j].legend(loc='best')
-# plt.show()
+mdl = 'BIRNN_attention'
+plot_cm_results(freqs, locs, [(key, value) for key, value in scenario_train_histories.items() if mdl in key], note=mdl)
+mdl = 'RNN'
+plot_cm_results(freqs, locs, [(key, value) for key, value in scenario_train_histories.items() if mdl in key], note=mdl)
--- utils.py
+++ utils.py
@@ -3,6 +3,10 @@ import matplotlib.pylab as plt
 from tensorflow.python.keras.backend import clear_session
 import tensorflow as tf
 from matplotlib.pyplot import cm
+from tensorflow.python.keras.layers import CuDNNLSTM, Concatenate
+
+from attention import Attention
+
 
 def window_slice(data, window_size, stride):
     assert window_size <= len(data)
@@ -12,7 +16,7 @@ def window_slice(data, window_size, stride):
         rtn.append(data[i - window_size:i])
     return np.array(rtn)
 
-def build_train_rnn(x_train, x_test, y_train, y_test, epochs=300, batch_size=64):
+def build_train_rnn(x_train, x_test, y_train, y_test, epochs=250, batch_size=64):
     clear_session()
     classifier = tf.keras.Sequential()
     classifier.add(tf.keras.layers.LSTM(units=64, return_sequences=True, input_shape=(x_train.shape[1:]), kernel_initializer='random_uniform', kernel_regularizer=tf.keras.regularizers.l2(l=1e-4)))
@@ -29,7 +33,7 @@ def build_train_rnn(x_train, x_test, y_train, y_test, epochs=300, batch_size=64)
     history = classifier.fit(x=x_train, y=y_train, validation_data=(x_test, y_test), epochs=epochs, batch_size=batch_size)
     return history
 
-def build_train_ann(x_train, x_test, y_train, y_test, epochs=300, batch_size=64):
+def build_train_ann(x_train, x_test, y_train, y_test, epochs=250, batch_size=64):
     clear_session()
 
     classifier = tf.keras.Sequential()
@@ -47,7 +51,7 @@ def build_train_ann(x_train, x_test, y_train, y_test, epochs=300, batch_size=64)
     return history
 
 
-def build_train_cnn(x_train, x_test, y_train, y_test, epochs=300, batch_size=64):
+def build_train_cnn(x_train, x_test, y_train, y_test, epochs=250, batch_size=64):
     clear_session()
     classifier = tf.keras.Sequential()
     classifier.add(tf.keras.layers.Conv1D(filters=16, kernel_size=(3,), input_shape=(x_train.shape[1:]), kernel_initializer='random_uniform', kernel_regularizer=tf.keras.regularizers.l2(l=1e-5)))
@@ -73,6 +77,47 @@ def build_train_cnn(x_train, x_test, y_train, y_test, epochs=300, batch_size=64)
     history = classifier.fit(x=x_train, y=y_train, validation_data=(x_test, y_test), epochs=epochs, batch_size=batch_size)
     return history
 
+
+
+def build_train_birnn_with_attention(x_train, x_test, y_train, y_test, epochs=250, batch_size=64):
+    clear_session()
+
+    sequence_input = tf.keras.layers.Input(shape=(x_train.shape[1], x_train.shape[2]))
+    lstm = tf.keras.layers.Bidirectional(CuDNNLSTM(64, return_sequences=True), name="bi_lstm_0")(sequence_input)
+    lstm = tf.keras.layers.Dropout(0.2, name="drop_0")(lstm)
+    # lstm = tf.keras.layers.Bidirectional(CuDNNLSTM(64, return_sequences=True), name="bi_lstm_1")(lstm)
+    # lstm = tf.keras.layers.Dropout(0.2, name="drop_1")(lstm)
+    # lstm = tf.keras.layers.Bidirectional(CuDNNLSTM(64, return_sequences=True), name="bi_lstm_2")(lstm)
+    # lstm = tf.keras.layers.Dropout(0.2, name="drop_2")(lstm)
+    (lstm, forward_h, forward_c, backward_h, backward_c) = tf.keras.layers.Bidirectional(
+        tf.keras.layers.LSTM(64, return_sequences=True, return_state=True), name="bi_lstm_1")(lstm)
+    state_h = Concatenate()([forward_h, backward_h])
+    state_c = Concatenate()([forward_c, backward_c])
+    context_vector, attention_weights = Attention(10)(lstm, state_h)
+    dense1 = tf.keras.layers.Dense(128, activation="relu")(context_vector)
+    dropout = tf.keras.layers.Dropout(0.05)(dense1)
+    output = tf.keras.layers.Dense(y_train.shape[1], activation="softmax")(dropout)
+
+    classifier = tf.keras.Model(inputs=sequence_input, outputs=output)
+    adam = tf.keras.optimizers.Adam(lr=1e-4, decay=1e-7)
+    classifier.compile(optimizer=adam, loss='categorical_crossentropy', metrics=['accuracy'])
+    history = classifier.fit(x=x_train, y=y_train, validation_data=(x_test, y_test), epochs=epochs, batch_size=batch_size)
+
+    # classifier = tf.keras.Sequential()
+    # classifier.add(tf.keras.layers.Bidirectional(CuDNNLSTM(units=64, return_sequences=True, input_shape=(x_train.shape[1:]), kernel_initializer='random_uniform', kernel_regularizer=tf.keras.regularizers.l2(l=1e-4))))
+    # classifier.add(tf.keras.layers.Dropout(0.2))  # ignore 20% of the neurons in both forward and backward propagation
+    # classifier.add(tf.keras.layers.Bidirectional(CuDNNLSTM(units=64, return_sequences=True, kernel_initializer='random_uniform', kernel_regularizer=tf.keras.regularizers.l2(l=1e-4))))
+    # classifier.add(tf.keras.layers.Dropout(0.2))  # ignore 20% of the neurons in both forward and backward propagation
+    # classifier.add(tf.keras.layers.Bidirectional(CuDNNLSTM(units=64, return_sequences=True, kernel_initializer='random_uniform', kernel_regularizer=tf.keras.regularizers.l2(l=1e-4))))
+    # classifier.add(tf.keras.layers.Dropout(0.2))
+    # classifier.add(tf.keras.layers.Attention())
+    # classifier.add(tf.keras.layers.Dense(units=128, kernel_initializer='random_uniform'))
+    # classifier.add(tf.keras.layers.Dropout(0.2))
+    # classifier.add(tf.keras.layers.Dense(units=y_train.shape[1], activation='softmax', kernel_initializer='random_uniform'))
+    # adam = tf.keras.optimizers.Adam(lr=1e-4, decay=1e-7)
+    # classifier.compile(optimizer=adam, loss='categorical_crossentropy', metrics=['accuracy'])
+    # history = classifier.fit(x=x_train, y=y_train, validation_data=(x_test, y_test), epochs=epochs, batch_size=batch_size)
+    return history
 
 def plot_train_history(history, note=''):
     plt.plot(history.history['accuracy'])
