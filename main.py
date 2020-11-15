d88a9ef50e94fba3dd41f49c97f77109a0d783df
apocalyvec <s-vector.lee@hotmail.com>
2020-11-14 23:37:41 -0500

    add plot roc, fix cm axis, clip cm colormpa

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import math
 import os
 import pickle
 import numpy as np
@@ -5,8 +6,10 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler, OneHotEncoder
 from scipy import interpolate
+from tensorflow.python.keras import models
+
 from utils import window_slice, build_train_rnn, plot_train_history, plot_cm_results, build_train_cnn, build_train_ann, \
-    build_train_birnn_with_attention
+    build_train_birnn_with_attention, get_img_from_fig, plot_roc_multiclass
 import matplotlib.pyplot as plt
 
 
@@ -128,15 +131,20 @@ for scn, xy in scenario_data.items():
 
 strcts = {'ANN': build_train_ann, 'RNN': build_train_rnn, 'CNN': build_train_cnn, 'BIRNN_attention': build_train_birnn_with_attention}
 freqs = ['04g', '24g', '5g']
-locs = ['foot', 'hand', 'head']
+locs = ['hea', 'han', 'foo']
+# reorder
+scenario_train_histories_reorder = {}
+for L in locs:
+    for F in freqs:
+        for mdl in strcts.keys():
+            scenario_train_histories_reorder[(mdl, (L, F))] = scenario_train_histories[(mdl, (L, F))]
+
 # horizontal axis are the frequencies, vertical are the locations
 for mdl in strcts.keys():
-    plot_cm_results(freqs, locs, [(key, value) for key, value in scenario_train_histories.items() if mdl in key], note=mdl)
+    plot_cm_results(freqs, locs, [(key, value) for key, value in scenario_train_histories_reorder.items() if mdl in key], note=mdl)
 
-scenario_train_histories_without_model = ([mdl_scn, his_eval[0].history] for mdl_scn, his_eval in scenario_train_histories.items())
-# pickle.dump(scenario_train_histories, open('scenario_train_histories_10012020.p', 'wb'))
-
-
+# scenario_train_histories_without_model = list([mdl_scn, his_eval[0].history] for mdl_scn, his_eval in scenario_train_histories.items())
+# pickle.dump(scenario_train_histories_without_model, open('results/scenario_train_histories_111420.p', 'wb'))
 
 
 
@@ -145,26 +153,33 @@ scenario_train_histories_without_model = ([mdl_scn, his_eval[0].history] for mdl
 
 
 
-# rename_map = {'foo': 'foot', 'han': 'hand', 'hea': 'head'}
-# scenario_train_histories_rename = dict()
-# for key, value in scenario_train_histories.items():
-#     key_rename = (key[0], (rename_map[key[1][0]], key[1][1]))
-#     scenario_train_histories_rename[key_rename] = value
-#
-# # plot scenario-based performance
-# width = 0.175
-# plt.rcParams["figure.figsize"] = (8, 4)
-# SMALL_SIZE = 12
-# MEDIUM_SIZE = 14
-# BIGGER_SIZE = 16
-#
-# plt.rc('font', size=SMALL_SIZE)  # controls default text sizes
-# plt.rc('axes', titlesize=SMALL_SIZE)  # fontsize of the axes title
-# plt.rc('axes', labelsize=MEDIUM_SIZE)  # fontsize of the x and y labels
-# plt.rc('xtick', labelsize=SMALL_SIZE)  # fontsize of the tick labels
-# plt.rc('ytick', labelsize=SMALL_SIZE)  # fontsize of the tick labels
-# plt.rc('legend', fontsize=SMALL_SIZE)  # legend fontsize
-# plt.rc('figure', titlesize=BIGGER_SIZE)
+
+
+rename_map = {'foo': 'foot', 'han': 'hand', 'hea': 'head'}
+rename_freq_map = {'04g': '0.4 GHz', '24g': '2.4 GHz', '5g': '5 GHz'}
+
+freqs = ['0.4 GHz', '2.4 GHz', '5 GHz']
+
+
+scenario_train_histories_rename = dict()
+for key, value in scenario_train_histories.items():
+    key_rename = (key[0], (rename_map[key[1][0]], rename_freq_map[key[1][1]]))
+    scenario_train_histories_rename[key_rename] = value
+
+# plot scenario-based performance
+width = 0.175
+plt.rcParams["figure.figsize"] = (8, 4)
+SMALL_SIZE = 12
+MEDIUM_SIZE = 14
+BIGGER_SIZE = 16
+
+plt.rc('font', size=SMALL_SIZE)  # controls default text sizes
+plt.rc('axes', titlesize=SMALL_SIZE)  # fontsize of the axes title
+plt.rc('axes', labelsize=MEDIUM_SIZE)  # fontsize of the x and y labels
+plt.rc('xtick', labelsize=SMALL_SIZE)  # fontsize of the tick labels
+plt.rc('ytick', labelsize=SMALL_SIZE)  # fontsize of the tick labels
+plt.rc('legend', fontsize=SMALL_SIZE)  # legend fontsize
+plt.rc('figure', titlesize=BIGGER_SIZE)
 # for scns in [locs, freqs]:
 #     strct_sns_acc_dict = dict([(model_strct, []) for model_strct in
 #                                strcts.keys()])  # i.e., ('ANN', [0.9, 0.8, 0.3]) in the list are the average accuracy for this sencario, sencario could be 04g
@@ -188,20 +203,152 @@ scenario_train_histories_without_model = ([mdl_scn, his_eval[0].history] for mdl
 #     plt.legend(loc=4)
 #     plt.show()
 #
-# # plot scenarios-based training history
-# plt.rcParams["figure.figsize"] = (10, 10)
-#
-# fig, ax = plt.subplots(nrows=len(locs), ncols=len(freqs))
-# for i, lc in enumerate(locs):
-#     for j, fq in enumerate(freqs):
-#         strct_sn_hists = [(strct_scn[0], hist_eval[0].history['val_loss']) for strct_scn, hist_eval in
-#                           scenario_train_histories_rename.items() if (lc, fq) == strct_scn[1]]
-#         strct_sn_hists.reverse()
-#         for strct, sn_hist in strct_sn_hists:
-#             ax[i][j].plot(sn_hist, label=strct)
-#         ax[i][j].set_xlabel('Epoch')
-#         ax[i][j].set_ylabel('Validation loss')
-#         ax[i][j].set_ylim(-0.1, 3.0)
-#         ax[i][j].set_title(str(lc) + ', ' + str(fq))
-#         ax[i][j].legend(loc='best')
-# plt.show()
+# plot scenarios-based training history ##################################################################
+plt.rcParams["figure.figsize"] = (15, 15)
+SMALL_SIZE = 18
+MEDIUM_SIZE = 20
+BIGGER_SIZE = 22
+
+plt.rc('font', size=SMALL_SIZE)  # controls default text sizes
+plt.rc('axes', titlesize=SMALL_SIZE)  # fontsize of the axes title
+plt.rc('axes', labelsize=MEDIUM_SIZE)  # fontsize of the x and y labels
+plt.rc('xtick', labelsize=SMALL_SIZE)  # fontsize of the tick labels
+plt.rc('ytick', labelsize=SMALL_SIZE)  # fontsize of the tick labels
+plt.rc('legend', fontsize=SMALL_SIZE)  # legend fontsize
+plt.rc('figure', titlesize=BIGGER_SIZE)
+
+fig, ax = plt.subplots(nrows=len(locs), ncols=len(freqs))
+for i, lc in enumerate(locs):
+    for j, fq in enumerate(freqs):
+        strct_sn_hists = [(strct_scn[0], hist_eval[0].history['val_accuracy']) for strct_scn, hist_eval in
+                          scenario_train_histories_rename.items() if (lc, fq) == strct_scn[1]]
+        for strct, sn_hist in strct_sn_hists:
+            ax[i][j].plot(sn_hist, label=strct)
+        ax[i][j].set_xlabel('Epoch')
+        ax[i][j].set_ylabel('Validation accuracy')
+        # ax[i][j].set_ylim(-0.1, 3.0)
+        ax[i][j].set_title(str(lc) + ', ' + str(fq))
+        # ax[i][j].legend(loc='best')
+plt.show()
+
+# visualize intermidiate CNN kernels ############################
+# first sample ###############
+scn = ('han', '24g')
+clss = 'sta'
+np.random.seed(3)
+dataset = scenario_data[scn]
+sample = dataset['x'][np.random.choice(np.where(dataset['y'] == clss)[0])]
+sample_normalized = sc.transform(sample)
+sample_normalized_batch = np.expand_dims(sample_normalized, axis=0)
+
+plt.rcParams["figure.figsize"] = (8, 4)
+fig, ax1 = plt.subplots()
+color = 'tab:red'
+ax1.plot(sample[:, 0], color=color)
+ax1.set_xlabel('Timesteps')
+ax1.set_ylabel('Phase', color=color)
+
+
+color = 'tab:blue'
+ax2 = ax1.twinx()
+ax2.plot(sample[:, 1], color=color)
+ax2.set_xlabel('Timesteps')
+ax2.set_ylabel('RSS', color=color)
+
+fig.tight_layout()
+plt.title('Scenario: ' + rename_map[scn[0]] + ',' + rename_freq_map[scn[1]] + '; ' + clss)
+plt.show()
+
+# get corresponding model
+model = scenario_train_histories[('CNN', scn)][0].model
+
+# xy = scenario_data[scn]
+# x = np.array([sc.transform(x_raw) for x_raw in xy['x']])
+# y = encoder.transform(xy['y'].reshape(-1, 1)).toarray()
+# x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.20, random_state=3, shuffle=True)
+# eval = model.evaluate(x=x_test, y=y_test)
+
+classes = model.predict(sample_normalized_batch)
+print("Predicted class is:", encoder.inverse_transform(classes))
+
+# Creates a model that will return these outputs, given the model input
+layer_outputs = [layer.output for layer in model.layers[:9] if 'batch_normalization' not in layer.name]
+# Extracts the outputs of the top 12 layers
+activation_model = models.Model(inputs=model.input, outputs=layer_outputs)
+activations = activation_model.predict(sample_normalized_batch)
+
+layer_names = []
+for layer in model.layers[:9]:
+    if 'batch_normalization' not in layer.name:
+        layer_names.append(layer.name)  # Names of the layers, so you can have them as part of your plot
+
+first_layer_activation = activations[0]
+print(first_layer_activation.shape)
+plt.plot(first_layer_activation[0, :, 0])
+plt.show()
+
+images_per_row = 8
+dpi = 180
+sub_fig_size = 5
+for layer_name, layer_activation in zip(layer_names, activations): # Displays the feature maps
+    n_features = layer_activation.shape[-1] # Number of features in the feature map
+    size = dpi * sub_fig_size
+
+    n_cols = n_features // images_per_row # Tiles the activation channels in this matrix
+    display_grid = np.zeros((size * n_cols, images_per_row * size, 3), dtype=int)
+    for col in range(n_cols): # Tiles each filter into a big horizontal grid
+        for row in range(images_per_row):
+            channel_image = layer_activation[0, :, col * images_per_row + row]
+            fig = plt.figure(figsize=(sub_fig_size, sub_fig_size))
+            ax = fig.add_subplot(111)
+            plt.plot(channel_image, linewidth = 3)
+            plt.axis('off')
+            channel_image = get_img_from_fig(fig, dpi=dpi)
+            display_grid[col * size : (col + 1) * size, # Displays the grid
+                         row * size : (row + 1) * size, :] = channel_image
+            plt.clf()
+    scale = 1. / size
+    plt.figure(figsize=(scale * display_grid.shape[1],
+                        scale * display_grid.shape[0]))
+    plt.title(layer_name)
+    plt.grid(False)
+    plt.xticks([])
+    plt.yticks([])
+    plt.imshow(display_grid, aspect='auto', cmap='viridis')
+    plt.show()
+# visualize Attention weights  ############################
+
+# plot ROC curve for biRNN model  ############################
+
+plt.rcParams["figure.figsize"] = (15, 15)
+SMALL_SIZE = 14
+MEDIUM_SIZE = 16
+BIGGER_SIZE = 18
+
+plt.rc('font', size=SMALL_SIZE)  # controls default text sizes
+plt.rc('axes', titlesize=SMALL_SIZE)  # fontsize of the axes title
+plt.rc('axes', labelsize=MEDIUM_SIZE)  # fontsize of the x and y labels
+plt.rc('xtick', labelsize=SMALL_SIZE)  # fontsize of the tick labels
+plt.rc('ytick', labelsize=SMALL_SIZE)  # fontsize of the tick labels
+plt.rc('legend', fontsize=SMALL_SIZE)  # legend fontsize
+plt.rc('figure', titlesize=BIGGER_SIZE)
+
+fig, ax = plt.subplots(nrows=len(locs), ncols=len(freqs))
+for i, scn_xy in enumerate(scenario_data.items()):
+    scn, xy = scn_xy
+    print('Plotting scenario: ' + str(scn))
+    x = np.array([sc.transform(x_raw) for x_raw in xy['x']])
+    y = encoder.transform(xy['y'].reshape(-1, 1)).toarray()
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.20, random_state=3, shuffle=True)
+
+    model = scenario_train_histories[('BIRNN_attention', scn)][0].model
+    eval = model.evaluate(x=x_test, y=y_test)
+    y_score = model.predict(x_test)
+
+    plot_roc_multiclass(n_classes=3, y_score=y_score, y_test=y_test, ax=ax[((math.floor(i / 3)) % 3)][i % 3], zoom=True)
+    # for strct, sn_hist in strct_sn_hists:
+    #     ax[i][j].plot(sn_hist, label=strct)
+    ax[((math.floor(i / 3)) % 3)][i % 3].set_xlabel('Epoch')
+    ax[((math.floor(i / 3)) % 3)][i % 3].set_ylabel('Validation accuracy')
+    ax[((math.floor(i / 3)) % 3)][i % 3].set_title(rename_map[scn[0]] + ', ' + rename_freq_map[scn[1]])
+plt.show()
--- utils.py
+++ utils.py
@@ -19,11 +19,11 @@ def window_slice(data, window_size, stride):
 def build_train_rnn(x_train, x_test, y_train, y_test, epochs=250, batch_size=64):
     clear_session()
     classifier = tf.keras.Sequential()
-    classifier.add(tf.keras.layers.LSTM(units=64, return_sequences=True, input_shape=(x_train.shape[1:]), kernel_initializer='random_uniform', kernel_regularizer=tf.keras.regularizers.l2(l=1e-4)))
+    classifier.add(CuDNNLSTM(units=64, return_sequences=True, input_shape=(x_train.shape[1:]), kernel_initializer='random_uniform', kernel_regularizer=tf.keras.regularizers.l2(l=1e-4)))
     classifier.add(tf.keras.layers.Dropout(0.2))  # ignore 20% of the neurons in both forward and backward propagation
-    classifier.add(tf.keras.layers.LSTM(units=64, return_sequences=True, kernel_initializer='random_uniform', kernel_regularizer=tf.keras.regularizers.l2(l=1e-4)))
+    classifier.add(CuDNNLSTM(units=64, return_sequences=True, kernel_initializer='random_uniform', kernel_regularizer=tf.keras.regularizers.l2(l=1e-4)))
     classifier.add(tf.keras.layers.Dropout(0.2))  # ignore 20% of the neurons in both forward and backward propagation
-    classifier.add(tf.keras.layers.LSTM(units=64, return_sequences=False, kernel_initializer='random_uniform', kernel_regularizer=tf.keras.regularizers.l2(l=1e-4)))
+    classifier.add(CuDNNLSTM(units=64, return_sequences=False, kernel_initializer='random_uniform', kernel_regularizer=tf.keras.regularizers.l2(l=1e-4)))
     classifier.add(tf.keras.layers.Dropout(0.2))
     classifier.add(tf.keras.layers.Dense(units=128, kernel_initializer='random_uniform'))
     classifier.add(tf.keras.layers.Dropout(0.2))
@@ -141,7 +141,7 @@ def plot_cm_results(freqs, locs, train_histories, note=''):
     acc_matrix = np.array([entry[1][1] for scn, entry in train_histories]).reshape((3, 3))
 
     fig, ax = plt.subplots()
-    im = ax.imshow(acc_matrix)
+    im = ax.imshow(acc_matrix,vmin=0, vmax=1)
 
     # We want to show all ticks...
     ax.set_xticks(np.arange(len(freqs)))
@@ -163,3 +163,160 @@ def plot_cm_results(freqs, locs, train_histories, note=''):
     ax.set_title("Three motion classification accuracies " + note, size=15)
     # fig.tight_layout()
     plt.show()
+
+import io
+import cv2
+
+# define a function which returns an image as numpy array from figure
+def get_img_from_fig(fig, dpi=180):
+    buf = io.BytesIO()
+    fig.savefig(buf, format="png", dpi=dpi)
+    buf.seek(0)
+    img_arr = np.frombuffer(buf.getvalue(), dtype=np.uint8)
+    buf.close()
+    img = cv2.imdecode(img_arr, 1)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+    return img
+
+from sklearn.metrics import roc_curve, auc
+from scipy import interp
+from itertools import cycle
+
+def plot_roc_multiclass(n_classes, y_score, y_test, ax=None, zoom=False):
+    # Plot linewidth.
+    lw = 2
+
+    # Compute ROC curve and ROC area for each class
+    fpr = dict()
+    tpr = dict()
+
+    roc_auc = dict()
+    for i in range(n_classes):
+        fpr[i], tpr[i], _ = roc_curve(y_test[:, i], y_score[:, i])
+        roc_auc[i] = auc(fpr[i], tpr[i])
+    # Compute micro-average ROC curve and ROC area
+    fpr["micro"], tpr["micro"], _ = roc_curve(y_test.ravel(), y_score.ravel())
+    roc_auc["micro"] = auc(fpr["micro"], tpr["micro"])
+
+    # Compute macro-average ROC curve and ROC area
+
+    # First aggregate all false positive rates
+    all_fpr = np.unique(np.concatenate([fpr[i] for i in range(n_classes)]))
+
+    # Then interpolate all ROC curves at this points
+    mean_tpr = np.zeros_like(all_fpr)
+    for i in range(n_classes):
+        mean_tpr += interp(all_fpr, fpr[i], tpr[i])
+
+    # Finally average it and compute AUC
+    mean_tpr /= n_classes
+
+    fpr["macro"] = all_fpr
+    tpr["macro"] = mean_tpr
+    roc_auc["macro"] = auc(fpr["macro"], tpr["macro"])
+
+    # Plot all ROC curves
+    if ax is None:
+        plt.figure(1)
+        plt.plot(fpr["micro"], tpr["micro"],
+                 label='micro-average ROC curve (area = {0:0.2f})'
+                       ''.format(roc_auc["micro"]),
+                 color='deeppink', linestyle=':', linewidth=4)
+
+        plt.plot(fpr["macro"], tpr["macro"],
+                 label='macro-average ROC curve (area = {0:0.2f})'
+                       ''.format(roc_auc["macro"]),
+                 color='navy', linestyle=':', linewidth=4)
+
+        colors = cycle(['aqua', 'darkorange', 'cornflowerblue'])
+        for i, color in zip(range(n_classes), colors):
+            plt.plot(fpr[i], tpr[i], color=color, lw=lw,
+                     label='ROC curve of class {0} (area = {1:0.2f})'
+                           ''.format(i, roc_auc[i]))
+
+        plt.plot([0, 1], [0, 1], 'k--', lw=lw)
+        plt.xlim([0.0, 1.0])
+        plt.ylim([0.0, 1.05])
+        plt.xlabel('False Positive Rate')
+        plt.ylabel('True Positive Rate')
+        plt.title('Some extension of Receiver operating characteristic to multi-class')
+        plt.legend(loc="lower right")
+        plt.show()
+
+        # Zoom in view of the upper left corner.
+        plt.figure(2)
+        plt.xlim(0, 0.2)
+        plt.ylim(0.8, 1)
+        plt.plot(fpr["micro"], tpr["micro"],
+                 label='micro-average ROC curve (area = {0:0.2f})'
+                       ''.format(roc_auc["micro"]),
+                 color='deeppink', linestyle=':', linewidth=4)
+
+        plt.plot(fpr["macro"], tpr["macro"],
+                 label='macro-average ROC curve (area = {0:0.2f})'
+                       ''.format(roc_auc["macro"]),
+                 color='navy', linestyle=':', linewidth=4)
+
+        colors = cycle(['aqua', 'darkorange', 'cornflowerblue'])
+        for i, color in zip(range(n_classes), colors):
+            plt.plot(fpr[i], tpr[i], color=color, lw=lw,
+                     label='ROC curve of class {0} (area = {1:0.2f})'
+                           ''.format(i, roc_auc[i]))
+
+        plt.plot([0, 1], [0, 1], 'k--', lw=lw)
+        plt.xlabel('False Positive Rate')
+        plt.ylabel('True Positive Rate')
+        plt.title('Some extension of Receiver operating characteristic to multi-class')
+        plt.legend(loc="lower right")
+        plt.show()
+    else:
+        if zoom:
+            # Zoom in view of the upper left corner.
+            ax.set_xlim(0, 0.2)
+            ax.set_ylim(0.8, 1)
+            ax.plot(fpr["micro"], tpr["micro"],
+                     label='micro-average ROC curve (area = {0:0.2f})'
+                           ''.format(roc_auc["micro"]),
+                     color='deeppink', linestyle=':', linewidth=4)
+
+            ax.plot(fpr["macro"], tpr["macro"],
+                     label='macro-average ROC curve (area = {0:0.2f})'
+                           ''.format(roc_auc["macro"]),
+                     color='navy', linestyle=':', linewidth=4)
+
+            colors = cycle(['aqua', 'darkorange', 'cornflowerblue'])
+            for i, color in zip(range(n_classes), colors):
+                ax.plot(fpr[i], tpr[i], color=color, lw=lw,
+                         label='ROC curve of class {0} (area = {1:0.2f})'
+                               ''.format(i, roc_auc[i]))
+
+            ax.plot([0, 1], [0, 1], 'k--', lw=lw)
+            ax.set_xlabel('False Positive Rate')
+            ax.set_ylabel('True Positive Rate')
+            # ax.legend()
+
+        else:
+            ax.plot(fpr["micro"], tpr["micro"],
+                     label='micro-average ROC curve (area = {0:0.2f})'
+                           ''.format(roc_auc["micro"]),
+                     color='deeppink', linestyle=':', linewidth=4)
+
+            ax.plot(fpr["macro"], tpr["macro"],
+                     label='macro-average ROC curve (area = {0:0.2f})'
+                           ''.format(roc_auc["macro"]),
+                     color='navy', linestyle=':', linewidth=4)
+
+            colors = cycle(['aqua', 'darkorange', 'cornflowerblue'])
+            for i, color in zip(range(n_classes), colors):
+                ax.plot(fpr[i], tpr[i], color=color, lw=lw,
+                         label='ROC curve of class {0} (area = {1:0.2f})'
+                               ''.format(i, roc_auc[i]))
+
+            ax.plot([0, 1], [0, 1], 'k--', lw=lw)
+            ax.set_xlim([0.0, 1.0])
+            ax.set_ylim([0.0, 1.05])
+            ax.set_xlabel('False Positive Rate')
+            ax.set_ylabel('True Positive Rate')
+            # ax.legend()
+
